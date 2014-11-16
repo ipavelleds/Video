a5afc8fbb53b7a73bfc9cb5807f8049c4b1673c4
Pavel Shaforostov <ipavelleds@gmail.com>
2014-11-16 17:12:14 +0600

    add static version

--- video_server/video_server/settings.py
+++ video_server/video_server/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_ROOT = os.path.join(PUBLIC_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/v1/'
 
 try:
     from local_settings import *
