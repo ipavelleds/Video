5a2a6d99d84267bad3cd2f9cd2e9cafe8ef957a8
ipavelleds@gmail.com <ipavelleds@gmail.com>
2014-11-28 14:28:57 +0600

    Add send request to email

--- video_server/landing/views.py
+++ video_server/landing/views.py
@@ -2,6 +2,9 @@
 
 from django.shortcuts import render, redirect
 from models import Request
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.mail import send_mail
 
 
 def validate_email(email):
@@ -33,4 +36,25 @@ def send_request(request):
     if errors:
         return render(request, 'index.html', {"old_post": old_post, "errors": errors})
     Request(name=old_post['name'], phone=old_post['phone'], email=old_post['email']).save()
+
+    super_users = User.objects.filter(is_superuser=True)
+    recipients = [super_user.email for super_user in super_users]
+    if recipients == [u'']:
+        recipients = [settings.EMAIL_HOST_USER]
+    user_name = ""
+    user_email = ""
+    if request.method == 'POST':
+        if request.POST["phone"]:
+            if old_post['name']:
+                user_name = u'Имя клиента: ' + old_post['name'] + '\n'
+            if old_post['email']:
+                user_email = u'Почта клиента: ' + old_post['email'] + '\n'
+            message = user_name + user_email + u'Телефон клиента: ' + old_post['phone']
+            send_mail(
+                'Новая заявка c Наблюдай.Онлайн',
+                message,
+                settings.EMAIL_HOST_USER,
+                recipients,
+                fail_silently=False
+            )
     return redirect('/')
